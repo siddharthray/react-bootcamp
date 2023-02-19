6022e6a0d4f8fe9684940d84e5741f1d8da49c1c
Siddharth Ray <siddharth.ray23@gmail.com>
2023-02-19 22:48:06 +0530

    initial commit

---
+++ react-bootcamp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h2">REACT JS</Typography>
+      <p>Hello John</p>
+      <p>Hello Sri</p>
+    </Container>
+  );
+}
+
+
+
+export default App;
